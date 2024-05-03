1e2c1954c4c96f609342599a0904b81f0f226b0f
Alperen Üstün <alperen.ustun39@gmail.com>
2024-05-03 22:10:48 +0300

    feat: Add loading spinner to Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,5 +1,11 @@
+import "./Loading.scss";
+
 const Loading: React.FC = () => {
-  return <div>Loading characters...</div>;
+  return (
+    <div className="loading-container">
+      <span className="loading-spinner"></span>
+    </div>
+  );
 };
 
 export default Loading;
